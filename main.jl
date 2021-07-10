4cbe3ccbb1f024d642c32da0dc8eb38e5b1f44f7
Maria Belen Amat <belen.amat29@gmail.com>
2021-07-10 00:30:31 -0300

    un pequeño paso para belu un gran paso para la humanidad

--- main.jl
+++ main.jl
@@ -1,15 +1,30 @@
 include("profile.jl")
 include("needleman.jl")
 
-str1 = "AGTA"
+function agregarStringVacioAMatriz(W, cantidadFilas, cantidadColumnas)
+    println(cantidadFilas)
+    println(cantidadColumnas)
+    for m in 0:cantidadFilas - 1
+        W[m + 1, 1] = -1 * m
+    end
+
+    for j in 0:cantidadColumnas - 1
+        W[1, j + 1] = -1 * j
+    end
+
+    return W
+end
+
+
+str1 = "ACGT"
 profile = inicializarProfile(str1)
 
-str2 = "AGTA"
+str2 = "GGAAGT"
 m = length(str1)
 n = length(str2)
 
-W = fill(typemin(Int64), m, n)
+W = fill(typemin(Int64), m , n )
+needle_top_down(profile, str2, m, n, W)
+W
 
-s = needle_top_down(profile, str2, m, n, W)
-println(W)
 #reconstruccion_top_down(str1, str2, W)
--- needleman.jl
+++ needleman.jl
@@ -1,26 +1,23 @@
 function needle_top_down(profile, str2, m, n, W)
     #esto devuelve un score y AFECTA a la matriz. Pero NO DEVUELVE UNA MATRIZ
+    #m es el index para la cadena del profile y n es el index para la cadena a comparar
 
-    if m == 1
-        W[m,n] = scoreColumna(profile, m, str2[m])
-        return W[m,n]
+    if m == 0
+        return n * -1
     end
 
-    if n == 1
-        W[m,n] = scoreColumna(profile, n, str2[n])
-        return W[m,n]
+    if n == 0
+        return m * -1
     end
 
     if W[m, n] != typemin(Int64)
         return W[m, n]
     end
 
-    #falta actualizar los porcentajes para llamar a las recursiones
-
     W[m, n] = max(
-            needle_top_down(profile, str2, m-1, n, W) + sii(str2[n]=="-", 0, -1), #gap contra gap es 0 y gap contra letra es -1
-            needle_top_down(profile, str2, m, n-1, W) + sii(str2[m]=="-", 0, -1),
-            needle_top_down(profile, str2, m-1, n-1, W) + scoreColumna(profile, n, str2[m]) #apareamiento de columna
+            needle_top_down(profile, str2, m-1, n, W) -1, #gap contra gap es 0 y gap contra letra es -1
+            needle_top_down(profile, str2, m, n-1, W) -1,
+            needle_top_down(profile, str2, m-1, n-1, W) + scoreColumna(profile, m, str2[n]) #apareamiento de columna
             )
 
     return W[m, n]
@@ -39,7 +36,6 @@ function compararEnMatriz(letra1, letra2)
     return(m[letrasYPosiciones[string(letra1)], letrasYPosiciones[string(letra2)]])
 end
 
-compararEnMatriz("-", "-")
 function sii(condicion, res1, res2)
     if condicion
         res1
@@ -59,12 +55,6 @@ function scoreColumna(profile, numeroColumna, caracterSegundaCadena) #no se si e
 
     return scoreDeColumna
 end
-
-str1 = "ACGT"
-str2 = "ACGA"
-profile = inicializarProfile(str1)
-
-scoreColumna(profile, 1, "A")
 
 function scoregap(porcentajes, letras)
      score = 0
