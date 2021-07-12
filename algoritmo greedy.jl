6484d8a57d0b172cb6aeb59f23bd3e1df33b8033
Maria Belen Amat <belen.amat29@gmail.com>
2021-07-12 19:27:51 -0300

    score con algoritmo greedy

--- algoritmo greedy.jl
+++ algoritmo greedy.jl
@@ -5,29 +5,31 @@ using(Combinatorics)
 function alineamientoGreedy(secuencias)
     mejorParInicial = encontrarMejorParInicial(secuencias) #buscamos los dos que mejor score tienen para empezar
     profileOriginal = actualizarProfile(mejorParInicial[3], mejorParInicial[4]) #armo el primer profile con el resultado del mejor par
-    listaSecuencias = eliminarDeListaDeSecuencias(mejorParInicial[1], secuencias) #elimino las secuencias del par inicial de la lista de secuencias restantes
+    listaSecuencias = eliminarSecuenciaDeLista(mejorParInicial[1][1], secuencias) #elimino las secuencias del par inicial de la lista de secuencias restantes
+    listaSecuencias = eliminarSecuenciaDeLista(mejorParInicial[1][2], secuencias)
 
     copiaProfile = profileOriginal
     score = mejorParInicial[2]
     while(listaSecuencias != []) #mientras queden cadenas sin alinear
-        mejorSecuenciaQueSigue = encontrarMejorSecuenciaQueSigue(listaSecuencias, copiaProfile)
+        mejorSecuenciaQueSigue = encontrarMejorSecuenciaQueSigue(listaSecuencias, copiaProfile) #busco la mejor cadena que sigue
         copiaProfile = actualizarProfile(copiaProfile, mejorSecuenciaQueSigue[1]) #agrego la secuencia al profile
         score += mejorSecuenciaQueSigue[2] #sumo el score del alineamiento
         listaSecuencias = eliminarSecuenciaDeLista(mejorSecuenciaQueSigue[1], listaSecuencias) #sacamos a la secuencia elegida de la lista de secuencias
     end
-    #FALTA EL VALOR A DEVOLVER, HABRIA QUE IR CONSTRUYENDO EL ALINEAMIENTO A MEDIDA QUE AVANZAMOS EN LA LISTA
-end
 
-alineamientoGreedy(["AACGT", "GTT-", "AAGTT", "AAGTA"])
+    return score #falta alineamiento, hay que ir armandolo en cada pasada del while (funcion reconstruccion_top_down)
+end
 
 function encontrarMejorParInicial(secuencias)
     listaDePares = collect(combinations(secuencias, 2)) #genera todos los pares posibles
     scoreMejorPar = typemin(Float64) #arranco con el numero mas pequeño posible
-    mejorPar = nothing
+    mejorPar = nothing #aun no tengo mi mejor par
+    profilePar = nothing
+    string2 = nothing
 
     for par in listaDePares
-        profilePar = inicializarProfile(par[1])
-        string2 = par[2]
+        profilePar = inicializarProfile(par[1]) #inicializo un nuevo profile con el primer elemento de cada par
+        string2 = par[2] #uso al segundo elemento de cada par como cadena
         lProfile = largoProfile(profile)
         W = fill(typemin(Float64), lProfile, length(string2))
         scorePar = needle_top_down(profilePar, string2, lProfile, length(string2), W) #averiguo el score de este par
@@ -43,8 +45,6 @@ end
 function eliminarSecuenciaDeLista(secuenciaAEliminar, listaSecuencias)
     return filter!(secuencia -> secuencia !== secuenciaAEliminar, listaSecuencias)
 end
-
-function
 
 function encontrarMejorSecuenciaQueSigue(secuencias, profile)
     mejorSecuencia = secuencias[1] #elijo la primera para empezar a comparar
@@ -63,3 +63,5 @@ function encontrarMejorSecuenciaQueSigue(secuencias, profile)
 
     return (mejorSecuencia, scoreMejorSecuencia)
 end
+
+alineamientoGreedy(["AACGT", "GTT-", "AAGTT", "AAGTA"])
