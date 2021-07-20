5238d740c8744f7f98f0f3a3a9f361b16b237433
Maria Belen Amat <belen.amat29@gmail.com>
2021-07-20 20:10:06 -0300

    se escriben resultados en archivod de salida

--- grasp_version2.jl
+++ grasp_version2.jl
@@ -4,7 +4,9 @@ include("cadena.jl")
 include("busquedaLocal.jl")
 include("parseo_de_cadenas.jl")
 
-function grasp(nombreArchivoCadenas, cantidadIteracionesMaximas, cantMejorasBusquedaLocal)
+using DelimitedFiles
+
+function grasp(nombreArchivoCadenas, nombreArchivoResultado, cantidadIteracionesMaximas, cantMejorasBusquedaLocal)
     matriz = matrizDeAminoacidos()
     iteracion = 0
 
@@ -25,6 +27,7 @@ function grasp(nombreArchivoCadenas, cantidadIteracionesMaximas, cantMejorasBusq
     maximoResultado = getMaximoResultado(mejoresResultadosBusquedasLocales)
     println("/////////// MEJOR RESULTADO GRASP: ", maximoResultado.score, " ///////////")
 
+    guardarResultadoEnArchivo(maximoResultado, nombreArchivoResultado)
     return maximoResultado
 end
 
@@ -42,7 +45,30 @@ function getMaximoResultado(mejoresResultadosBusquedasLocales)
     return mejorResultado
 end
 
+function guardarResultadoEnArchivo(resultado, nombreArchivoDestino)
+    file = open(nombreArchivoDestino, "a")
+
+    write(file, "\n")
+    write(file, "-----------------------------------------------------------------")
+    write(file, "\n")
+    write(file, "Ultimo alineamiento que hiciste:")
+    write(file, "\n")
+    for cadena in resultado.profile.profileListaDeCadenas
+        write(file, cadena)
+        write(file, "\n")
+    end
+
+    write(file, "\n")
+    write(file, "Mejor score:")
+    write(file, "\n")
+    write(file, string(resultado.score))
+    write(file, "\n")
+    write(file, "-----------------------------------------------------------------")
+    close(file)
+end
+
 result = grasp("archivo_de_cadenas.txt",
-10,
-10
+"achivo_resultado.txt",
+1,
+2
 )
